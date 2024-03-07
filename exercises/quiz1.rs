d1411eb4492e8925059fae668dcf322160effbe4
Rafał Safin <rafal.safin@rafsaf.pl>
2024-03-07 14:10:02 +0100

    quiz1
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(order: i32) -> i32 {
+    if order <= 40 {
+        return order * 2;
+    }
+    order
+}
 
 // Don't modify this function!
 #[test]
